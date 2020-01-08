e36d04e4b8683ddfc36909ce32ac694b84a67ce4
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-08 13:29:49 -0700

    added api keys as environment variables

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import SearchBar from "./SearchBar";
 import "./App.css";
+require("dotenv").config();
 
 export class App extends Component {
+  onSearchSubmit(term) {}
+
   render() {
+    console.log(process.env);
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onSubmit={this.onSearchSubmit} />
       </div>
     );
   }
