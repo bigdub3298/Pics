bd3fa5206fc12a891be2fc62e93267db83b38d15
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-09 14:02:29 -0700

    added network functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
 import "./App.css";
 require("dotenv").config();
 
 export class App extends Component {
-  onSearchSubmit(term) {}
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
+    });
+
+    this.setState({ images: response.data.results });
+  };
 
   render() {
-    console.log(process.env);
     return (
       <div className="ui container">
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 
 class SearchBar extends Component {
+  state = {
+    term: ""
+  };
+
+  onFormSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit(this.state.term);
+  };
+
   render() {
     return (
       <div className="search-bar ui segment">
-        <form className="ui form">
+        <form className="ui form" onSubmit={this.onFormSubmit}>
           <div className="field">
             <label>Image Search</label>
             <input
               type="text"
               placeholder="What would you like to see?"
               id="search-term"
+              value={this.state.term}
+              onChange={e => this.setState({ term: e.target.value })}
             />
           </div>
           <button className="ui button" type="submit">
