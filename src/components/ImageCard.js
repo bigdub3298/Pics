13474ba006153fd4a27743a722635fc4afe4b716
bigdub3298 <wesleyaustin33@gmail.com>
2020-01-09 23:13:47 -0700

    fixed grid spacing

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,11 +1,32 @@
 import React, { Component } from "react";
 
 export class ImageCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.imageRef = React.createRef();
+    this.state = {
+      spans: 0
+    };
+  }
+
+  componentDidMount() {
+    this.imageRef.current.addEventListener("load", this.setSpans);
+  }
+
+  setSpans = () => {
+    const height = this.imageRef.current.clientHeight;
+
+    const spans = Math.ceil(height / 10);
+
+    this.setState({ spans });
+  };
+
   render() {
-    const { description, url } = this.props.image;
+    const { description, urls } = this.props.image;
     return (
-      <div>
-        <img src={url.regular} alt={description} />
+      <div style={{ gridRowEnd: `span ${this.state.spans}` }}>
+        <img ref={this.imageRef} src={urls.regular} alt={description} />
       </div>
     );
   }
